84cf3ca1202da347a474f18728be85f313ffb1d8
dgavrilov <gavrilov.dmitriy04@gmail.com>
2023-03-22 00:14:28 +0100

    task 3-11

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
@@ -13,7 +14,7 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private final Map<String, Integer> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +22,12 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.merge(restaurantName, 1, Integer::sum);
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat.entrySet().stream()
+                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                .collect(Collectors.toSet());
     }
 }
